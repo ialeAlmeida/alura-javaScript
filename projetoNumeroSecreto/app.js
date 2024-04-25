35c1af56ea8d53fe4c71274d3c9ca93d5f5b2e3c
Iale Almeida <almeidaialesantos@gmail.com>
2024-04-25 17:53:17 -0300

    Adicinadas as atividades propostas no primeiro módulo

--- projetoNumeroSecreto/app.js
+++ projetoNumeroSecreto/app.js
@@ -2,6 +2,20 @@ alert("Bem vindo ao jogo do Número Secreto!!! ;)");
 let secretNum = 5;
 let answer = prompt("Esolha um número entre 1 e 10");
 
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert("Erro! Preencha todos os campos");
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+nome = prompt("Digite seu nome: ");
+idade = prompt("Digite sua idade: ");
+
+if (idade >= 18) {
+    alert("Pode tirar a habilitação!");
+}
+
 if (secretNum == answer) {
     console.log("Parabéns! Você descobriu o número secreto (" + answer + ")");
 } else if (answer < secretNum) {
