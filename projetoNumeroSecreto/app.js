9b894909f43376c72969a764ca93ba016a45ca3d
Iale Almeida <almeidaialesantos@gmail.com>
2024-04-25 19:39:35 -0300

    Atualizações do segundo módulo: Inclusão de Template String

--- projetoNumeroSecreto/app.js
+++ projetoNumeroSecreto/app.js
@@ -1,25 +1,11 @@
 alert("Bem vindo ao jogo do Número Secreto!!! ;)");
-let secretNum = 5;
+let secretNum = 9;
 let answer = prompt("Esolha um número entre 1 e 10");
 
-let nome = "Lua";
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-alert("Erro! Preencha todos os campos");
-let mensagemDeErro = "Erro! Preencha todos os campos";
-alert(mensagemDeErro);
-nome = prompt("Digite seu nome: ");
-idade = prompt("Digite sua idade: ");
-
-if (idade >= 18) {
-    alert("Pode tirar a habilitação!");
-}
-
 if (secretNum == answer) {
-    console.log("Parabéns! Você descobriu o número secreto (" + answer + ")");
+    alert(`Parabéns! Você descobriu o número secreto (${secretNum})`);
 } else if (answer < secretNum) {
-    console.log("Resposta incorreta. O número sercreto é maior do que " + answer + ". Tente de novo");
+    alert(`Resposta incorreta. O número sercreto é maior do que ${answer}. Tente de novo`);
 } else if (answer > secretNum) {
-    console.log("Resposta incorreta. O número sercreto é menor do que " + answer + ". Tente de novo");
+    alert(`Resposta incorreta. O número sercreto é menor do que ${answer}. Tente de novo`);
 }
