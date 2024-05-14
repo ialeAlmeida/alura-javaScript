bc07a12ea674dffd4053a2696a70bec9703af2e2
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-14 15:49:37 -0300

    First exercise finished

---
+++ formacao-aprenda-a-programar-em-java-script-com-foco-no-back-end/variaveis/exercicios.js
@@ -0,0 +1,7 @@
+const string = 'casa';
+const num = 51;
+const moraAi = true;
+
+console.log(typeof string);
+console.log(typeof num);
+console.log(typeof moraAi);
---
+++ formacao-aprenda-a-programar-em-java-script-com-foco-no-back-end/variaveis/variaveis.js
@@ -0,0 +1,13 @@
+//var é permissimo, consegue criar uma varável já com valor ou não
+var aluna = 'Ana';
+var aluno;
+
+aluna = 'Vanessa';
+//let é parecida com var, mas difere quando se é declarada em escopos de bloco ou funções.
+//Pois nesses casos, não pode ser acessada fora desses escopos.
+let professora = 'Fraancisca';
+let professor;
+
+professora = 'Mariana';
+//const é declaração de variável constante(não pode alterar) e não pode iniciar sem valor
+const escola = 'Desafio';
