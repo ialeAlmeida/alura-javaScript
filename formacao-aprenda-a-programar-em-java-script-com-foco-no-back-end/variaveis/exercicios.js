b19fc490e4f7c07de080429b6b283166fa48dcc3
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-14 15:54:23 -0300

    Second exercise finished

--- formacao-aprenda-a-programar-em-java-script-com-foco-no-back-end/variaveis/exercicios.js
+++ formacao-aprenda-a-programar-em-java-script-com-foco-no-back-end/variaveis/exercicios.js
@@ -5,3 +5,18 @@ const moraAi = true;
 console.log(typeof string);
 console.log(typeof num);
 console.log(typeof moraAi);
+
+//--------------------------------------------------------------
+
+const primeiroNome = 'Iale ';
+const ultimoNome = "Almeida";
+const nomesJuntos = primeiroNome + ultimoNome;
+const nomesJuntosTempStr = `${primeiroNome}${ultimoNome}`;
+
+console.log(primeiroNome);
+console.log(ultimoNome);
+console.log(nomesJuntos);
+console.log(nomesJuntosTempStr);
+
+//--------------------------------------------------------------
+
