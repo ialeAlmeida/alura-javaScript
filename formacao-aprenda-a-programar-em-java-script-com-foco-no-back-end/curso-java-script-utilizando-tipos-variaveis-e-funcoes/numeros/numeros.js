d8f2a2223037c133d24e1ede14df46edf8d23543
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-15 16:10:35 -0300

    iniciando módulo de números

---
+++ formacao-aprenda-a-programar-em-java-script-com-foco-no-back-end/curso-java-script-utilizando-tipos-variaveis-e-funcoes/numeros/numeros.js
@@ -0,0 +1,8 @@
+const nota1 = 5.3;
+const nota2 = 9;
+const nota3 = 6.85;
+const nota4 = Number.parseInt('4');
+
+const total = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(total.toFixed(2));
